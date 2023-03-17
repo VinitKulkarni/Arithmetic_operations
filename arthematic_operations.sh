69efb947671aaabe8570cdcdda125becbcd6c4ee
vinit <vinitkkulkarni@gmail.com>
2023-03-17 10:26:13 +0530

    added substraction code

--- arthematic_operations.sh
+++ arthematic_operations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "YOU CAN PERFORM ARITHEMATIC OPERATION"
+echo "*** YOU CAN PERFORM ARITHEMATIC OPERATION ***"
 
 a=10;
 b=5;
@@ -8,3 +8,7 @@ b=5;
 #addition code
 result_add=$(($a+$b))
 echo "addition result: $result_add"
+
+#substraction code
+result_substract=$(($a-$b))
+echo "substraction result: $result_substract"
